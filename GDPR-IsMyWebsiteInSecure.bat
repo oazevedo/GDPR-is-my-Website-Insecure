d6f1eda767ce2a01450ac323d1802ad3720d12ee
oazevedo <oscar.msazevedo@gmail.com>
2024-05-03 11:26:22 +0100

    Update GDPR-IsMyWebsiteInSecure.bat

--- GDPR-IsMyWebsiteInSecure.bat
+++ GDPR-IsMyWebsiteInSecure.bat
@@ -7,7 +7,7 @@ rem ===========================================================
 rem developed by Oscar Azevedo
 rem oscar.azevedo@aeportugal.pt, oscar.msazevedo@gmail.com
 rem run webtools to see if website is insecure
-rem modified on: 2024-05-01
+rem modified on: 2024-05-03
 rem ===========================================================
 
 setlocal enableextensions
@@ -24,8 +24,10 @@ echo.
 echo. ===========================================================
 echo.   Is my Website InSecure?                               
 echo.                                                         
-echo.   1. Test Website                                       
-echo.   2. Choose browser (optional)                          
+echo.   1. Test Website - essential tools (default)           
+echo.   2. Test Website - all tools                           
+echo.                                                         
+echo.   3. Choose browser (optional)                          
 echo.                                                         
 echo.   F. Free Webtools used and Metrics                     
 echo.   L. Legal and Privacy Terms                            
@@ -36,7 +38,7 @@ echo.   websites whose owners have given you permission to do
 echo.   so. In addition, always make a vulnerability analysis 
 echo.   (ex. OWASP ZAP). This is a Free Tool.                 
 echo.                                                         
-echo.  (c)2024 r1.9 Oscar Azevedo                             
+echo.  (c)2024 r2.0 Oscar Azevedo                             
 echo. ===========================================================
 echo.
 
@@ -53,12 +55,14 @@ echo. Option   = 1. Test Website
 echo. ==========================
 echo.
 
-rem choice /c 12FLX /n /m " Choose an option (1,2,F,L,X) ? "
-choice /t 10 /c 12FLX /d 1 /n /m " Choose an option (1,2,F,L,X) ? "
-goto Label-%ERRORLEVEL%
+choice /t 10 /c 123FLX /d 1 /n /m " Choose an option (1,2,3,F,L,X) ? "
+
+(set option=%ERRORLEVEL%)
+goto Label-%option%
 
 
-:Label-1    Test Website
+:Label-1    Test Website, essencial tools
+:Label-2    Test Website, all tools
 echo.
 
 rem url = scheme + host + port + ..., ex. https://www.aeportugal.com 
@@ -104,14 +108,18 @@ echo.
 timeout /t -1
 
 
-start %browser% -new-window -%private% "https://checkcybersecurity.service.ncsc.gov.uk/ip-check/form"
+start %browser% -new-window -%private% -disable-extensions %url%
 timeout /t 2 >nul
 
+if "%option%"=="2" (
+start %browser% -new-tab -%private% "https://checkcybersecurity.service.ncsc.gov.uk/ip-check/form"
+timeout /t 2 >nul
+)
+
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://checkcybersecurity.service.ncsc.gov.uk/browser-check/form"
 timeout /t 2 >nul
-
-start %browser% -new-tab -%private% %url%
-timeout /t 2 >nul
+)
 
 start %browser% -new-tab -%private% "https://pentest-tools.com/network-vulnerability-scanning/tcp-port-scanner-online-nmap/"
 timeout /t 2 >nul
@@ -122,8 +130,10 @@ timeout /t 2 >nul
 start %browser% -new-tab -%private% "https://sitecheck.sucuri.net/results/%host%"
 timeout /t 2 >nul
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://haveibeenpwned.com/"
 timeout /t 2 >nul
+)
 
 start %browser% -new-tab -%private% "https://powerdmarc.com/dkim-record-lookup/"
 timeout /t 2 >nul
@@ -131,8 +141,10 @@ timeout /t 2 >nul
 start %browser% -new-tab -%private% "https://domain-checker.valimail.com/dmarc/%domain%"
 timeout /t 2 >nul
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://checkcybersecurity.service.ncsc.gov.uk/email-security-check/results?domain=%domain%"
 timeout /t 2 >nul
+)
 
 start %browser% -new-tab -%private% "https://www.ssllabs.com/ssltest/analyze.html?d=%url%&hideResults=on&latest"
 timeout /t 2 >nul
@@ -140,8 +152,10 @@ timeout /t 2 >nul
 start %browser% -new-tab -%private% "https://securityheaders.com/?followRedirects=on&hide=on&q=%url%"
 timeout /t 2 >nul
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://csp-evaluator.withgoogle.com/?csp=%url%"
 timeout /t 2 >nul
+)
 
 start %browser% -new-tab -%private% "https://report-uri.com/"
 timeout /t 2 >nul
@@ -149,23 +163,33 @@ timeout /t 2 >nul
 start %browser% -new-tab -%private% "https://observatory.allizom.org/analyze/%host%?third-party=false"
 timeout /t 2 >nul
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://webcheck.pt/pt/dns/loading.php?domain=%domain%"
 timeout /t 2 >nul
+)
 
 start %browser% -new-tab -%private% "https://internet.nl/site/%host%/"
 timeout /t 2 >nul
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://owasp.org/www-project-top-ten/"
 timeout /t 2 >nul
+)
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://www.immuniweb.com/darkweb/"
 timeout /t 2 >nul
+)
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://www.immuniweb.com/cloud/"
 timeout /t 2 >nul
+)
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://www.immuniweb.com/email/"
 timeout /t 2 >nul
+)
 
 start %browser% -new-tab -%private% "https://www.immuniweb.com/websec/"
 timeout /t 2 >nul
@@ -176,35 +200,47 @@ timeout /t 2 >nul
 start %browser% -new-tab -%private% "https://www.zaproxy.org/"
 timeout /t 2 >nul
 
-start %browser% -new-tab -%private% "https://portswigger.net/burp/communitydownload"
+if "%option%"=="2" (
+start %browser% -new-tab -%private% "https://portswigger.net/burp/pro"
 timeout /t 2 >nul
+)
 
 start %browser% -new-tab -%private% "https://www.tenable.com/products/nessus/nessus-essentials"
 timeout /t 2 >nul
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://community.greenbone.net/getting-started/greenbone-community-edition-via-linux-distribution-packages/"
 timeout /t 2 >nul
+)
 
 start %browser% -new-tab -%private% "https://snyk.io/product/snyk-code/"
 timeout /t 2 >nul
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://cisofy.com/lynis/"
 timeout /t 2 >nul
+)
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://learn.cisecurity.org/cis-cat-lite"
 timeout /t 2 >nul
+)
 
 start %browser% -new-tab -%private% "https://www.cookiebot.com/en/compliance-test/?domain=%host%"
 timeout /t 2 >nul
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://hub.sovy.com/gdpr-scan/?domain=%host%"
 timeout /t 2 >nul
+)
 
 start %browser% -new-tab -%private% "https://developers.google.com/speed/pagespeed/insights/?url=%url%"
 timeout /t 2 >nul
 
+if "%option%"=="2" (
 start %browser% -new-tab -%private% "https://gtmetrix.com/?url=%url%"
 timeout /t 2 >nul
+)
 
 start %browser% -new-tab -%private% "https://validator.w3.org/nu/?showsource=no&doc=%url%/"
 timeout /t 2 >nul
@@ -215,7 +251,7 @@ timeout /t 2 >nul
 goto :end
 
 
-:Label-2    Choose Browser
+:Label-3    Choose Browser
 @echo.
 choice /n /c bcefx /m "  Choose the browser: (B)rave, (C)hrome, (E)dge, (F)irefox ? "
 if ERRORLEVEL 1 (set browser=brave.exe)
@@ -225,13 +261,13 @@ if ERRORLEVEL 4 (set browser=firefox.exe)
 goto :menu
 
 
-:Label-3      Free Webtools and Metrics
+:Label-4      Free Webtools and Metrics
 @echo.
 @echo. We use the following free web tools. Our thanks and appreciation to those who develop and maintain.
 @echo.
 @echo. 1. category:security, target:hosting
 @echo.    "https://pentest-tools.com"                  metric: tcp/443
-@echo.    "https://www.wpsec.com/"                     metric: no vulnerabilities, no outdated plugins       
+@echo.    "https://www.wpsec.com/"                     metric: no vulnerabilities, no outdated plugins
 @echo.    "https://sitecheck.sucuri.net/"              metric: no malware, no outdated software
 @echo.    "https://haveibeenpwned.com/"                metric: no email in a data breach
 @echo.    "https://domain-checker.valimail.com/dmarc"  metric: dmarc configured
@@ -244,7 +280,7 @@ goto :menu
 @echo.    "https://www.owasp.org/"                     metric: no vulnerabilities
 @echo.    "https://owasp.org/www-project-top-ten/"     metric: n/a
 @echo.    "https://www.immuniweb.com/darkweb/"         metric: no Dark Web Exposure
-@echo.   
+@echo.
 @echo. 2. category:GDPR cookies, target:developers
 @echo.    "https://www.ezigdpr.com/"         metric: no action required
 @echo.
@@ -258,7 +294,7 @@ pause
 goto :menu
 
 
-:Label-4     Legal & Privacy
+:Label-5     Legal & Privacy
 @echo.
 @echo. Very Important:
 @echo.
@@ -274,7 +310,7 @@ pause
 goto :menu
 
 
-:Label-5
+:Label-6
 :end
 (set url=)
 (set scheme=)
@@ -283,4 +319,5 @@ goto :menu
 (set domain=)
 (set browser=)
 (set private=)
+(set option=)
 endlocal
